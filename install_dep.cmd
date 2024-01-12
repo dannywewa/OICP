8d3d5b51d710f3229615a293a54fc0edb2e4b677
dannywewa <danny.wang@tetaprecision.com>
2024-01-12 15:04:29 -0800

    add jupyter notebooks for mtf and 4-color

--- install_dep.cmd
+++ install_dep.cmd
@@ -3,13 +3,17 @@ pip install ifaddr
 pip install pyee
 pip install opencv-contrib-python
 pip install numpy
+pip install scipy
 pip install PyYAML
 pip install typing_extensions
 pip install zeroconf
 pip install iniconfig
+pip install matplotlib
 pip install "fastapi[all]"
 pip install "uvicorn[standard]"
 pip install "C:/Program Files/Allied Vision/Vimba X/api/python/vmbpy-1.0.3-py3-none-any.whl"
+
+pip install pillow
 
 pip install jupyterlab
 pip install asynctest
