95479744b7817ab3ed33410bab0c15b7fe136aa1
dannywewa <danny.wang@tetaprecision.com>
2024-01-12 12:04:53 -0800

    add package installation script

---
+++ install_dep.cmd
@@ -0,0 +1,22 @@
+pip install jsonschema
+pip install ifaddr
+pip install pyee
+pip install opencv-contrib-python
+pip install numpy
+pip install PyYAML
+pip install typing_extensions
+pip install zeroconf
+pip install iniconfig
+pip install "fastapi[all]"
+pip install "uvicorn[standard]"
+pip install "C:/Program Files/Allied Vision/Vimba X/api/python/vmbpy-1.0.3-py3-none-any.whl"
+
+pip install jupyterlab
+pip install asynctest
+pip install coverage
+pip install flake8
+pip install mypy
+pip install mypy-extensions
+pip install packaging
+pip install pluggy
+
